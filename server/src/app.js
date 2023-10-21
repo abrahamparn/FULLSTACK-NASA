6bbbb90650b90d3ac7817ad0c608f46b0df5632c
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-10-21 23:10:03 +0700

    adding planets endpoing with its controller and cors module

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,23 @@
 // WE WILL PUT ALL OF OUR EXPRESS CODE HERE
-
 const express = require('express')
+const cors = require('cors')
+
+const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
+
+// const whiteList = ['http://localhost']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (whiteList.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+//   }
+app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json())
+app.use(planetsRouter)
 
 module.exports = app;
