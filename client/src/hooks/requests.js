2308bd790ed50e7704d6940fc8fe302e928b9b86
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-12-14 22:09:48 +0700

    adding mongodb

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,59 +1,50 @@
-const API_URL = 'http://localhost:5000'
+const API_URL = "http://localhost:8000";
 
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`)
-  return await response.json()
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
-  const response = await fetch(`${API_URL}/launches`)
-  const fetchedLaunches = await response.json()
-  return await fetchedLaunches.sort((a, b)=> {
-    return a.flightNumber - b.flightNumber
-  })
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return await fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
   // Load launches, sort by flight number, and return as JSON.
 }
 
 async function httpSubmitLaunch(launch) {
-  try{
+  try {
     return await fetch(`${API_URL}/launches`, {
-      method:'post',
-      headers:{
-        "Content-Type":"application/json"
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(launch),
-  
-    })
-  }catch(err){
+    });
+  } catch (err) {
     return {
       ok: false,
-    }
+    };
   }
-  
+
   // Submit given launch data to launch system.
 }
 
 async function httpAbortLaunch(id) {
-  try{
-
-    return await fetch(`${API_URL}/launches/${id}`,{
-      method:'delete',
-    })
-  }catch(err){
-    console.log(err)
-    return{
-
-      ok:false
-    }
-
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
   }
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,31 @@
-const http = require('http')
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
 
-const app = require('./app')
+const planetsModel = require("./models/planets.model");
 
-const planetsModel= require('./models/planets.model')
+const PORT = process.env.PORT || 8000; // we set flexible or 8000 because our client runs on 3000 so that it does not conflict.
 
-const PORT = process.env.PORT || 8000 // we set flexible or 8000 because our client runs on 3000 so that it does not conflict.
+const MONGO_URL =
+  "mongodb+srv://nasa-api:dqjegsLWA6zY09fC@atlascluster.rtopyd5.mongodb.net/nasa?retryWrites=true&w=majority";
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-async function startServer(){
-    await planetsModel.loadPlanetsData()
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connection Ready!");
+});
 
-    server.listen(PORT, ()=>{
-        console.log(`listening on port ${PORT}...`)
-    })  
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  await planetsModel.loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
+  });
 }
 
-startServer()
+startServer();
