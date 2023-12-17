516d2fd4954b384be42aa0b4a7cb5e7cadeccdb9
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-12-17 13:31:01 +0700

    checkpoint for learning jwt

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:dqjegsLWA6zY09fC@atlascluster.rtopyd5.mongodb.net/nasa?retryWrites=true&w=majority";
-
+const MONGO_URL = process.env.MONGO_URL;
 mongoose.connection.once("open", () => {
   console.log("MongoDB Connection Ready!");
 });
